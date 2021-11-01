2e0fe5744e460b53232dad44ccac80a173a8a39c
Boba <bobaruljic@gmail.com>
2021-11-01 23:15:04 +0100

    modal dialog added

---
+++ PatientFrontend/src/app/feedback-form/feedback-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-feedback-form',
+  templateUrl: './feedback-form.component.html',
+  styleUrls: ['./feedback-form.component.css']
+})
+export class FeedbackFormComponent implements OnInit {
+
+  constructor(public dialogRef: MatDialogRef<FeedbackFormComponent>) { }
+
+  ngOnInit(): void {
+  }
+
+}
